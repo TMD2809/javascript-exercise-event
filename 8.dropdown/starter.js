daa841fc678f2a7372b5cebf702b5b0b8b8cf246
tmd2809 <tangminhduy2809@gmail.com>
2023-07-24 14:36:51 +0700

    update 8.dropdown

--- 8.dropdown/starter.js
+++ 8.dropdown/starter.js
@@ -1,8 +1,9 @@
 const dropdownButton = document.querySelector(".dropdown__select");
-dropdownButton.addEventListener("click", showDropdown);
+const dropdownContent = document.querySelector(".dropdown__selected");
 const dropdownCaret = document.querySelector(".dropdown__caret");
+const dropdownList = document.querySelector(".dropdown__list");
+dropdownButton.addEventListener("click", showDropdown);
 function showDropdown(e) {
-    const dropdownList = e.target.nextElementSibling;
     if (dropdownList) {
         dropdownList.classList.toggle("show");
         dropdownCaret.classList.toggle("fa-caret-down");
@@ -10,4 +11,11 @@ function showDropdown(e) {
     }
 }
 const dropdownBox = [...document.querySelectorAll(".dropdown__item")];
-dropdownBox.forEach;
+dropdownBox.forEach((item) => item.addEventListener("click", replaceTitle));
+function replaceTitle(e) {
+    const content = e.target.querySelector(".dropdown__text").textContent;
+    dropdownContent.textContent = content;
+    dropdownList.classList.remove("show");
+    dropdownCaret.classList.toggle("fa-caret-down");
+    dropdownCaret.classList.toggle("fa-caret-up");
+}
