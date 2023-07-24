da55ec4936a21683ee54164f49f7983c0d97caef
tmd2809 <tangminhduy2809@gmail.com>
2023-07-24 16:01:00 +0700

    update 8.dropdown

--- 8.dropdown/starter.js
+++ 8.dropdown/starter.js
@@ -2,20 +2,27 @@ const dropdownButton = document.querySelector(".dropdown__select");
 const dropdownContent = document.querySelector(".dropdown__selected");
 const dropdownCaret = document.querySelector(".dropdown__caret");
 const dropdownList = document.querySelector(".dropdown__list");
+const dropdown = document.querySelector(".dropdown");
+const dropdownBox = [...document.querySelectorAll(".dropdown__item")];
 dropdownButton.addEventListener("click", showDropdown);
-function showDropdown(e) {
+function dropboxToggle() {
+    dropdownList.classList.toggle("show");
+    dropdownCaret.classList.toggle("fa-caret-up");
+}
+function showDropdown() {
     if (dropdownList) {
-        dropdownList.classList.toggle("show");
-        dropdownCaret.classList.toggle("fa-caret-down");
-        dropdownCaret.classList.toggle("fa-caret-up");
+        dropboxToggle();
     }
 }
-const dropdownBox = [...document.querySelectorAll(".dropdown__item")];
 dropdownBox.forEach((item) => item.addEventListener("click", replaceTitle));
 function replaceTitle(e) {
     const content = e.target.querySelector(".dropdown__text").textContent;
     dropdownContent.textContent = content;
-    dropdownList.classList.remove("show");
-    dropdownCaret.classList.toggle("fa-caret-down");
-    dropdownCaret.classList.toggle("fa-caret-up");
+    dropboxToggle();
 }
+document.addEventListener("click", function (e) {
+    if (!dropdown.contains(e.target)) {
+        dropdownList.classList.remove("show");
+        dropdownCaret.classList.remove("fa-caret-up");
+    }
+});
