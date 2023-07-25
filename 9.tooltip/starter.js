16181ea3b9660c9070d4727f310fc1cbc170d67a
tmd2809 <tangminhduy2809@gmail.com>
2023-07-25 20:37:44 +0700

    update 9.tooltip

--- 9.tooltip/starter.js
+++ 9.tooltip/starter.js
@@ -7,5 +7,12 @@ window.addEventListener("load", function () {
         div.innerHTML = textPopup;
         div.className = "tooltip-text";
         document.body.appendChild(div);
+        const cords = e.target.getBoundingClientRect();
+        console.log(cords);
+        const divHeight = div.offsetHeight;
+        const triangleHeight = 20;
+        const { width, height, top, right, left } = cords;
+        div.style.left = `${left - width / 2}px`;
+        div.style.top = `${top - triangleHeight - divHeight}px`;
     }
 });
