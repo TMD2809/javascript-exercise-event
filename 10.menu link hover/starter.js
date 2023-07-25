915dc12ceed6e5a9cf6bbd8dc8bb378c6c5914b9
tmd2809 <tangminhduy2809@gmail.com>
2023-07-25 23:23:18 +0700

    update 9.tooltip

--- 10.menu link hover/starter.js
+++ 10.menu link hover/starter.js
@@ -1,14 +1,18 @@
 const menu = [...document.querySelectorAll(".menu-link")];
 menu.forEach((i) => i.addEventListener("mouseenter", hoverEnable));
+const menuContainer = document.querySelector(".menu");
 const div = document.createElement("div");
 div.className = "line-effect";
 document.body.appendChild(div);
 function hoverEnable(e) {
-    const hightLight = document.querySelector(".line-effect");
     const cord = e.target.getBoundingClientRect();
     const { width, top, left, height } = cord;
     const Blank = 5;
-    hightLight.style.width = `${width}px`;
-    hightLight.style.left = `${left}px`;
-    hightLight.style.top = `${top + height + Blank}px`;
+    div.style.width = `${width}px`;
+    div.style.left = `${left}px`;
+    div.style.top = `${top + height + Blank}px`;
+}
+menuContainer.addEventListener("mouseleave", hoverDisable);
+function hoverDisable() {
+    div.style.width = `0px`;
 }
