4be834f17715371c982695d754cde32a57391c72
tmd2809 <tangminhduy2809@gmail.com>
2023-07-25 20:50:39 +0700

    update 9.tooltip

--- 9.tooltip/starter.js
+++ 9.tooltip/starter.js
@@ -8,11 +8,15 @@ window.addEventListener("load", function () {
         div.className = "tooltip-text";
         document.body.appendChild(div);
         const cords = e.target.getBoundingClientRect();
-        console.log(cords);
         const divHeight = div.offsetHeight;
         const triangleHeight = 20;
         const { width, height, top, right, left } = cords;
         div.style.left = `${left - width / 2}px`;
         div.style.top = `${top - triangleHeight - divHeight}px`;
+    }
+    content.addEventListener("mouseleave", hideHover);
+    function hideHover(e) {
+        const tooltipText = document.querySelector(".tooltip-text");
+        tooltipText.parentNode.removeChild(tooltipText);
     }
 });
