d5538b0de820dcd06f91eeeab791ef22708a725f
tmd2809 <tangminhduy2809@gmail.com>
2023-08-01 14:07:51 +0700

    update highlight-text-search

--- highlight-text-search/starter.js
+++ highlight-text-search/starter.js
@@ -2,14 +2,19 @@ const dropdownItems = document.querySelectorAll(".dropdown-item");
 const input = document.querySelector(".input-search");
 input.addEventListener("input", handleSearch);
 function handleSearch(e) {
-    let inputContent = e.target.value.toLowerCase();
+    let inputContent = e.target.value;
+    inputContent = inputContent.toLowerCase();
     dropdownItems.forEach((i) => {
-        const contentResult = i.textContent.toLowerCase();
-        const index = contentResult.indexOf(inputContent);
-        if (index >= 0 && contentResult.startsWith(inputContent)) {
-            i.innerHTML = `<span class="primary">${inputContent}</span>${contentResult.substring(
-                inputContent.length
-            )}`;
+        const contentResult = i.textContent;
+        const index = contentResult.toLowerCase().indexOf(inputContent);
+        if (
+            index >= 0 &&
+            contentResult.toLowerCase().startsWith(inputContent)
+        ) {
+            i.innerHTML = `<span class="primary">${contentResult.substring(
+                index,
+                index + inputContent.length
+            )}</span>${contentResult.substring(inputContent.length)}`;
         }
     });
 }
