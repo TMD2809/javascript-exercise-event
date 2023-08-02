9154e201a253ca71724e72bf84ed021accefa22b
tmd2809 <tangminhduy2809@gmail.com>
2023-08-02 11:27:33 +0700

    update 15.fixed-menu

--- 15.fixed-menu/starter.js
+++ 15.fixed-menu/starter.js
@@ -1,11 +1,31 @@
-const header = document.querySelector(".header");
-const headerHeight = header.offsetHeight;
-window.addEventListener("scroll", menuFixed);
-function menuFixed(e) {
-    const scrollY = window.pageYOffset;
-    if (scrollY > headerHeight) {
-        header.classList.add("is-fixed");
-    } else {
-        header.classList.remove("is-fixed");
-    }
+function debounceFn(func, wait, immediate) {
+    let timeout;
+    return function () {
+        let context = this,
+            args = arguments;
+        let later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 }
+const header = document.querySelector(".header");
+const headerHeight = header && header.offsetHeight;
+
+window.addEventListener(
+    "scroll",
+    debounceFn(function menuFixed() {
+        const scrollY = window.pageYOffset;
+        if (scrollY > headerHeight) {
+            header && header.classList.add("is-fixed");
+            document.body.style.paddingTop = `${headerHeight}px`;
+        } else {
+            document.body.style.paddingTop = 0;
+            header && header.classList.remove("is-fixed");
+        }
+    }, 100)
+);
