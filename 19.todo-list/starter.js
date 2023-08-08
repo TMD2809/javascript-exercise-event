1f70cd0b2b7fa4460f76b79c10e6ecb6509fb893
tmd2809 <tangminhduy2809@gmail.com>
2023-08-08 09:55:34 +0700

    update 19.todo-list

--- 19.todo-list/starter.js
+++ 19.todo-list/starter.js
@@ -1,16 +1,40 @@
 const todoList = document.querySelector(".todo-list");
-const todoArr = [];
+// query data from local storage
+let todoArr =
+    localStorage.length > 0 ? JSON.parse(localStorage.getItem("todoList")) : [];
 const todoForm = document.querySelector(".todo-form");
+if (Array.isArray(todoArr) && todoArr.length > 0) {
+    todoArr.forEach((i) => addTemplate(i));
+}
+// function add template
+function addTemplate(title) {
+    const template = `<div class="todo-item">
+        <span class="todo-text">${title}</span>
+        <i class="fa fa-trash todo-remove" data-value="Learning english">`;
+    todoList.insertAdjacentHTML("beforeend", template);
+}
+// create todo item
 todoForm.addEventListener("submit", submitItem);
 function submitItem(e) {
     e.preventDefault();
     let todoContent = this.elements["todo"].value;
-    const template = `<div class="todo-item">
-        <span class="todo-text">${todoContent}</span>
-        <i class="fa fa-trash todo-remove" data-value="Learning english">`;
     if (todoContent === "") return;
-    todoList.insertAdjacentHTML("afterbegin", template);
-    // todoArr = todoArr.push(todoContent);
-    // localStorage.setItem("todoList", JSON.stringify(todoArr));
+    addTemplate(todoContent);
+    todoArr.push(todoContent);
+    localStorage && localStorage.setItem("todoList", JSON.stringify(todoArr));
     this.elements["todo"].value = "";
+}
+// remove todo item
+todoList.addEventListener("click", removeItem);
+function removeItem(e) {
+    if (e.target.matches(".todo-remove")) {
+        // remove in dom
+        const item = e.target.parentNode;
+        item.parentNode.removeChild(item);
+        // remove in local storage
+        let contentTodo = e.target.previousElementSibling.textContent;
+        const index = todoArr.findIndex((i) => i === contentTodo);
+        todoArr.splice(index, 1);
+        localStorage.setItem("todoList", JSON.stringify(todoArr));
+    }
 }
