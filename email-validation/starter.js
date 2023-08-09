e6cef07abd99499bf8daeb283cfa9977acbf6476
tmd2809 <tangminhduy2809@gmail.com>
2023-08-09 17:16:13 +0700

    update 21.email-validation

--- email-validation/starter.js
+++ email-validation/starter.js
@@ -3,14 +3,17 @@ const check = document.querySelector(".fa-check");
 const errorCheck = document.querySelector(".fa-times");
 input.addEventListener("input", handleMailValidation);
 function handleMailValidation(e) {
-    const emailInput = e.target.value.trim; //|| trim: remove white leading and trailing of input string
+    const emailInput = e.target.value.trim(); //|| trim: remove white leading and trailing of input string
     const regMail = /(\w)(@gmail.com)/;
     const regMailResult = regMail.test(emailInput);
-    if (regMailResult === true) {
-        errorCheck.style = ``;
-        check.style = `opacity: 1; visibility: visible; transform: scale(1)`;
-    } else if (regMailResult === false) {
-        check.style = ``;
-        errorCheck.style = `opacity: 1; visibility: visible; transform: scale(1)`;
+    if (regMailResult) {
+        e.target.classList.add("valid");
+        e.target.classList.remove("invalid");
+    } else {
+        e.target.classList.remove("valid");
+        e.target.classList.add("invalid");
+    }
+    if (!emailInput) {
+        e.target.classList.remove("invalid");
     }
 }
