cf52aba078818a3917e8628745f94fc65637b0d2
tmd2809 <tangminhduy2809@gmail.com>
2023-08-16 01:38:10 +0700

    update 25.simple-slider

--- 25.simple-slider/starter.js
+++ 25.simple-slider/starter.js
@@ -10,35 +10,32 @@ window.addEventListener("load", function () {
     const sliderItemWidth = sliderItem[1].offsetWidth;
     prevButton.addEventListener("click", function () {
         handleSlider(-1);
-        console.log("working");
     });
     nextButton.addEventListener("click", function () {
         handleSlider(1);
     });
+    function handleDotClick(position, activeIndex) {
+        sliderMain.style = `transform: translateX(${position}px)`;
+        sliderDotItem.forEach((i) => i.classList.remove("active"));
+        sliderDotItem[activeIndex].classList.add("active");
+    }
     function handleSlider(direction) {
         if (direction === 1) {
-            if (positionX <= -(sliderItem.length - 1) * sliderItemWidth) return;
+            if (positionX <= -(sliderItem.length - 1) * 1000) return;
             positionX = positionX - sliderItemWidth;
-            sliderMain.style = `transform: translateX(${positionX}px)`;
-            sliderDotItem.forEach((i) => i.classList.remove("active"));
-            sliderDotItem[(positionX * -1) / 1000].classList.add("active");
+            handleDotClick(positionX, (positionX * -1) / 1000);
         } else if (direction === -1) {
             if (positionX >= 0) return;
             positionX = positionX + sliderItemWidth;
-            sliderMain.style = `transform: translateX(${positionX}px)`;
-            sliderDotItem.forEach((i) => i.classList.remove("active"));
-            sliderDotItem[(positionX * -1) / 1000].classList.add("active");
+            handleDotClick(positionX, (positionX * -1) / 1000);
         }
     }
     sliderDotItem.forEach((i) => {
         i.addEventListener("click", sliderDot);
         function sliderDot(e) {
             const dataIndex = e.target.dataset.index;
-            sliderMain.style = `transform: translateX(${
-                -dataIndex * sliderItemWidth
-            }px)`;
-            sliderDotItem.forEach((i) => i.classList.remove("active"));
-            sliderDotItem[dataIndex].classList.add("active");
+            positionX = -dataIndex * sliderItemWidth;
+            handleDotClick(-dataIndex * sliderItemWidth, dataIndex);
         }
     });
 });
