8443a05151ea4111f45b01201afffd705074674b
tmd2809 <tangminhduy2809@gmail.com>
2023-09-19 18:26:18 +0700

    upload code

---
+++ IVVV.Prototype&Constructor function/constructorFunction.js
@@ -0,0 +1,18 @@
+//! syntax:
+function functionName(arguments, arguments1) {
+    this.arguments;
+    this.arguments1;
+}
+//! create new object by using constructor function
+const newObject = new functionName(newArguments, newArguments1);
+const newObject1 = new functionName(newArguments, newArguments1);
+//! using method in constructor function:
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    //|| Define a method using the constructor's prototype:
+    this.calculateBirthYear = function () {
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.age;
+    };
+}
